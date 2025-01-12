1cdc5b206fa748bc4b57ebee1bf54dd837b0a8ce
fikri fiuca <mis.stf17@gmail.com>
2025-01-12 14:46:40 +0700

    cp

---
+++ cmd.sh
@@ -0,0 +1,13 @@
+helm list -n kong
+helm upgrade kong-cp -n kong kong/kong --values values-cp.yaml
+helm template kong-cp kong/kong -n kong --values ./values-cp.yaml > kong-cp3.yaml
+
+kubectl exec -it kong-cp-kong-5cbcd4d875-plxwj -c proxy -n kong -- kong config db_import <<EOF
+_format_version: "1.1"
+rbac_users:
+- name: kong-cp
+  user_token: caco123
+  role: super-admin
+EOF
+
+sudo apt install apache2 # for httpd
