686699415e424aa7b35dd763b9f1e598c38eef10
fikri fiuca <mis.stf17@gmail.com>
2025-01-13 21:47:58 +0700

    limit ip address

--- cmd.sh
+++ cmd.sh
@@ -1,8 +1,17 @@
+gcloud config set account mis.stf17@gmail.com
+gcloud config configurations list
+gcloud config list
+gcloud info
+gcloud container clusters get-credentials caco-cluster1 --region=australia-southeast1
+
+
 helm list -n kong
 helm upgrade kong-cp -n kong kong/kong --values values-cp.yaml
 helm template kong-cp kong/kong -n kong --values ./values-cp.yaml > kong-cp3.yaml
 helm install kong-cp kong/kong -n kong --values ./values-cp.yaml
 
+helm template kong-dp kong/kong -n kong --values ./values-dp.yaml > kong-dp.yaml
+helm upgrade kong-dp kong/kong -n kong --values ./values-dp.yaml
 
 kubectl exec -it kong-cp-kong-5cbcd4d875-plxwj -c proxy -n kong -- kong config db_import <<EOF
 _format_version: "1.1"
